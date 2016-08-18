c71c685d09ebea8406b68b354ee9a3940636a9b6
christienaucoin <c--aucoin@hotmail.com>
2016-08-18 17:09:16 -0300

    Update linkedlist.sh

--- linkedlist.sh
+++ linkedlist.sh
@@ -16,38 +16,37 @@ clear
 	read -p "How many servers are you adding:  " op2
 	if [ $op2 == 2 ]; then
 		gluster volume create $op1 replica 2 gluster1:/zpool/vol1/brick gluster2:/zpool/vol2/brick gluster2:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 3 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 4 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 5 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 6 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 7 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 8 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 9 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster9:/zpool/vol2/brick gluster9:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
 	if [ $op2 == 10 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster9:/zpool/vol2/brick gluster9:/zpool/vol1/brick gluster10:/zpool/vol2/brick gluster10:/zpool/vol1/brick gluster1:/zpool/vol2/brick
-    	gluster volume info $op1
+		gluster volume info $op1
 	fi
-    ;;
