28bd2f8e142267f67d1bed444173921a61b089f5
christienaucoin <c--aucoin@hotmail.com>
2016-08-18 17:08:24 -0300

    Update addpair.sh

--- addpair.sh
+++ addpair.sh
@@ -4,26 +4,24 @@
 ##########
 #Adding to a Paired Server Gluster
 #
-rem=$(( $op6 % 2 ))
-add1=$(( $op6 + 1 ))
-add2=$(( $op6 + 2 ))
 
 clear
 	echo -e "This will only work if are capable of adding two servers to your topology and you've already Created the Bricks on these new servers.\n"
 	read -p "Do you want to continue? (Y/n) " yn2
 	case $yn2 in
-	[Yy] *)		
+	[Yy]*)		
 		read -p "What is the name of your Gluster Volume:  " op5
 		read -p "How many servers are currently in your Gluster? " op6
-		if [ $rem != 0 ]; then
-			echo -e "\n Not a valid number of servers in Gluster, Must be an even number.\n "
-			read -p "Press Enter to Try Again" con1
-			case $con1 in
-			*) 
-				read -p "How many servers are currently in your Gluster? " op6
-			;;
-			esac
-		fi 		
+		rem=$(( $op6 % 2 ))
+		add1=$(( $op6 + 1 ))
+		add2=$(( $op6 + 2 ))
+		until [ $rem == 0 ]; do
+			echo -e "\nNot a valid number of servers in Gluster, Must be an even number.\n " 
+			read -p "How many servers are currently in your Gluster? " op6
+			rem=$(( $op6 % 2 ))
+			add1=$(( $op6 + 1 ))
+			add2=$(( $op6 + 2 ))
+		done		
 		read -p "Press Enter to confirm that you want to add gluster$add1 and gluster$add2 to $op5 " con2
 		case $con2 in
 			*)
@@ -40,5 +38,5 @@ clear
 	[nN]*)
 			;;
 	esac
-	;;	
+
 	
