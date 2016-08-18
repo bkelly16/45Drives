34cf80caa0ec44cae96dd480404f05a4877470cb
christienaucoin <c--aucoin@hotmail.com>
2016-08-18 14:32:45 -0300

    Update linkedlist.sh

--- linkedlist.sh
+++ linkedlist.sh
@@ -14,39 +14,39 @@ clear
 	
 	read -p "Enter a name for your Gluster Volume:  " op1
 	read -p "How many servers are you adding:  " op2
-	if [ $op2 == 2 ]
+	if [ $op2 == 2 ]; then
 		gluster volume create $op1 replica 2 gluster1:/zpool/vol1/brick gluster2:/zpool/vol2/brick gluster2:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 3 ]
+	if [ $op2 == 3 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 4 ]
+	if [ $op2 == 4 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 5 ]
+	if [ $op2 == 5 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 6 ]
+	if [ $op2 == 6 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 7 ]
+	if [ $op2 == 7 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 8 ]
+	if [ $op2 == 8 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 9 ]
+	if [ $op2 == 9 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster9:/zpool/vol2/brick gluster9:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
-	if [ $op2 == 10 ]
+	if [ $op2 == 10 ]; then
 		gluster volume create $op1 replica 2 gluster1:/pool/vol1/brick gluster2:/pool/vol2/brick gluster2:/pool/vol1/brick gluster3:/pool/vol2/brick gluster3:/zpool/vol1/brick gluster4:/zpool/vol2/brick gluster4:/zpool/vol1/brick gluster5:/zpool/vol2/brick gluster5:/zpool/vol1/brick gluster6:/zpool/vol2/brick gluster6:/zpool/vol1/brick gluster7:/zpool/vol2/brick gluster7:/zpool/vol1/brick gluster8:/zpool/vol2/brick gluster8:/zpool/vol1/brick gluster9:/zpool/vol2/brick gluster9:/zpool/vol1/brick gluster10:/zpool/vol2/brick gluster10:/zpool/vol1/brick gluster1:/zpool/vol2/brick
     	gluster volume info $op1
 	fi
