5eb999ee32198b37e6c101740b98f5647c9bbbf0
Brett Kelly <bkelly@45drives.com>
2016-10-05 13:58:44 -0300

    Update

--- dmap.sh
+++ dmap.sh
@@ -1,18 +1,33 @@
-
-
+#!/bin/bash
 if [ -e /etc/zfs/vdev_id.conf.test ];then
         rm -f /etc/zfs/vdev_id.conf.test
 fi
 
-read -p "Disk Controller? (r750) " hba
-read -p "Chassis Type? (30,45, or 60) " chassis
-
-if [ "$hba" == "r750" ];then
-	hba="0750"
-elif [ "$hba" != "LSI" ];then
-	echo "Unsupported controller, try configurating manaully"
+if [ $# -eq 0 ]; then
+	read -p "Disk Controller? (r750) " hba
+	if [ "$hba" == "r750" ];then
+		hba="0750"
+	elif [ "$hba" != "LSI" ];then
+		echo "Unsupported controller, try configurating manaully"
+		exit
+	fi
+	read -p "Chassis Type? (30,45, or 60) " chassis
+else
+	hba=$1
+	chassis=$2
+	if [ "$hba" == "r750" ] || [ "$hba" == "R750" ] ;then
+		hba="0750"
+	elif [ "$hba" != "LSI" ];then
+		echo "Unsupported controller, try configurating manaully"
+		exit
+	fi
 fi
-
+if [ "$chassis" -eq 30 ] || [ "$chassis" -eq 45 ] || [ "$chassis" -eq 60 ];then
+	:
+else
+	echo "$chassis is not an available chassis size, (30,45, or 60)"
+fi
+ 
 case $chassis in
 30)	
 	card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
@@ -24,6 +39,7 @@ case $chassis in
         	echo "alias 1-$slot     /dev/disk/by-path/pci-0000:$card1-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf.test
         	let slot=slot+1
 	done
+	cat /etc/zfs/vdev_id.conf.test
 	;;
 
 45)
@@ -47,11 +63,11 @@ case $chassis in
         	fi
         	let slot=slot+1
 	done
+	cat /etc/zfs/vdev_id.conf.test
 	;;
 60)
 	card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
 	card2=$(lspci | grep $hba | awk 'NR==2{print $1}')
-	card2=$(lspci | grep $hba | awk 'NR==3{print $1}')
 	#Card 1
 	slot=1
 	echo "# by-vdev" >> /etc/zfs/vdev_id.conf.test
@@ -66,6 +82,7 @@ case $chassis in
         	echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf.test
         	let slot=slot+1
 	done
+	cat /etc/zfs/vdev_id.conf.test
 	;;
 esac
 
