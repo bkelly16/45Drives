26f0568e1928d19eb53eeeeda855000f74e3e208
Brett Kelly <bkelly@45drives.com>
2016-10-13 21:11:21 -0300

    added adaptec HBA1000-16i support

--- gtools_v1.1/dmap.sh
+++ gtools_v1.1/dmap.sh
@@ -6,9 +6,13 @@ fi
 
 if [ $# -eq 0 ]; then
 	read -p "Disk Controller? (r750) " hba
-	if [ "$hba" == "r750" ];then
+	if [ "$hba" == "r750" ] || [ "$hba" == "R750" ];then
 		hba="0750"
-	elif [ "$hba" != "LSI" ];then
+	elif [ "$hba" == "LSI" ];then
+		hba="LSI"
+	elif [ "$hba" == "Adaptec" ];then
+		hba="Adpatec"
+	else
 		echo "Unsupported controller, try configurating manually"
 		exit
 	fi
@@ -16,10 +20,14 @@ if [ $# -eq 0 ]; then
 else
 	hba=$1
 	chassis=$2
-	if [ "$hba" == "r750" ] || [ "$hba" == "R750" ] ;then
+	if [ "$hba" == "r750" ] || [ "$hba" == "R750" ] || "$hba" == "r" ] ;then
 		hba="0750"
-	elif [ "$hba" != "LSI" ];then
-		echo "Unsupported controller, try configurating manaully"
+	elif [ "$hba" == "LSI" ]|| [ "$hba" == "lsi" ] || "$hba" == "l" ] ;then
+		hba="LSI"
+	elif [ "$hba" == "Adaptec" ] || [ "$hba" == "a" ];then
+		hba="Adpatec"
+	else
+		echo "Unsupported controller, try configurating manually"
 		exit
 	fi
 fi
@@ -44,26 +52,55 @@ case $chassis in
 	;;
 
 45)
-	card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
-	card2=$(lspci | grep $hba | awk 'NR==2{print $1}')
-	#Card 1
-	slot=1
-	echo "# by-vdev" >> /etc/zfs/vdev_id.conf.test
-	echo "# name     fully qualified or base name of device link" >> /etc/zfs/vdev_id.conf.test
-	while [ $slot -lt 25 ];do
-        	echo "alias 1-$slot     /dev/disk/by-path/pci-0000:$card1-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
-        	let slot=slot+1
-	done
-	#Card 2
-	slot=1
-	while [ $slot -lt 22 ];do
-        	if [ $slot -eq 21 ];then
-        	        echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:0:23:0" >> /etc/zfs/vdev_id.conf
-        	else
-        	        echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
-        	fi
-        	let slot=slot+1
-	done
+	case $hba in
+	0750)	
+		card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
+		card2=$(lspci | grep $hba | awk 'NR==2{print $1}')
+		#Card 1
+		slot=1
+		while [ $slot -lt 25 ];do	
+        		echo "alias 1-$slot     /dev/disk/by-path/pci-0000:$card1-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
+        		let slot=slot+1
+		done
+		#Card 2
+		slot=1
+		while [ $slot -lt 22 ];do
+        		if [ $slot -eq 21 ];then
+        		        echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:0:23:0" >> /etc/zfs/vdev_id.conf
+        		else
+        		        echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:0:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
+        		fi
+        		let slot=slot+1
+		done
+		;;
+	Adaptec)
+		card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
+		card2=$(lspci | grep $hba | awk 'NR==2{print $1}')
+		card3=$(lspci | grep $hba | awk 'NR==3{print $1}')
+
+		#Card 1
+		slot=1
+		while [ $slot -lt 17 ];do	
+        		echo "alias 1-$slot     /dev/disk/by-path/pci-0000:$card1-scsi-0:2:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
+        		let slot=slot+1
+		done
+		#Card 2
+		slot=1
+		while [ $slot -lt 17 ];do	
+        		echo "alias 2-$slot     /dev/disk/by-path/pci-0000:$card2-scsi-0:2:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
+        		let slot=slot+1
+		done
+		#Card 3
+		slot=1
+		while [ $slot -lt 17 ];do	
+        		echo "alias 1-$slot     /dev/disk/by-path/pci-0000:$card3-scsi-0:2:$(expr $slot - 1):0" >> /etc/zfs/vdev_id.conf
+        		let slot=slot+1
+		done
+		;;
+	LSI)	
+		echo "Not implemented yet :("
+		;;
+	esac	
 	;;
 60)
 	card1=$(lspci | grep $hba | awk 'NR==1{print $1}')
