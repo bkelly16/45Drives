250ddf576446edc23682b3365de8162a347b701b
bkelly16 <bkelly@45drives.com>
2016-10-18 23:01:13 -0300

    Update perf.sh

--- perf/perf.sh
+++ perf/perf.sh
@@ -26,12 +26,12 @@ if [ $length -gt 1 ];then
 	w0=$(cat run_results | awk -v l="$(expr $length - 1 )" 'NR==l{print $6}')
 fi
 if [ $length -gt 1 ] && [ $r1 -lt $r0 ] && [ $w1 -lt $w0 ];then
-	sh bench.sh $i $bs $fs
+	sh bench.sh $i $bs $fs $3
 	echo "Saturation reached"
 	exit 1
 fi
 
-sh bench.sh $i $bs $fs
+sh bench.sh $i $bs $fs $3
 
 done
 
